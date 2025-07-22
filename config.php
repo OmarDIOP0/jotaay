12489f0acf1b013ff75bfa6795a4bb756af1bbc7
OmarDIOP0 <odiop6170@gmail.com>
2025-07-22 18:47:33 +0000

    implementaation de config pour charger les fichiers

--- config.php
+++ config.php
@@ -1,16 +1,13 @@
 <?php
 session_start();
 
-// Vérifier si l'utilisateur est connecté
 if (!isset($_SESSION['id_utilisateur'])) {
-    header('Location: connexion/login.php');
+    header('Location: auth/login.php');
     exit;
 }
 
-// Définir le chemin absolu de base
 $cheminBase = __DIR__ . '/xmls/';
 
-// Charger les données XML avec vérification
 $utilisateurs = @simplexml_load_file($cheminBase . 'users.xml');
 if ($utilisateurs === false) {
     die('Erreur : Impossible de charger users.xml. Vérifiez le fichier ou le chemin.');
@@ -26,15 +23,9 @@ if ($groupes === false) {
     die('Erreur : Impossible de charger groups.xml. Vérifiez le fichier ou le chemin.');
 }
 
-
-
 $messages = @simplexml_load_file($cheminBase . 'messages.xml');
 if ($messages === false) {
     die('Erreur : Impossible de charger messages.xml. Vérifiez le fichier ou le chemin.');
 }
 
-// var_dump($utilisateurs);
-// var_dump($contacts);
-// var_dump($groupes);
-// var_dump($messages);
 ?>
